12d7767c7ffd2478695e9abfc13adb0e0343bcbd
Dilan Livera <Dylan_yahampath@yahoo.com>
2022-01-29 16:05:25 +1100

    initial commit
    - Add solution and project
    - Add editorconfig
    - Add readme file
    - Add interfaces and dispatchers

---
+++ mediatr-clone-demo/README.md
@@ -0,0 +1,9 @@
+# MediatR clone
+
+## Credits
+
+- [Why I don't use MediatR for CQRS](https://cezarypiatek.github.io/post/why-i-dont-use-mediatr-for-cqrs/)
+
+## Resources
+
+- [MediatR](https://github.com/jbogard/MediatR)
---
+++ mediatr-clone-demo/src/MediatR/interfaces/MediatR.cs
@@ -0,0 +1,23 @@
+﻿namespace MediatR;
+
+public interface IQueryHandler<in TQuery, TQueryResult>
+{
+    Task<TQueryResult> Handle(TQuery query, CancellationToken cancellation);
+}
+
+public interface IQueryDispatcher
+{
+    Task<TQueryResult> Dispatch<TQuery, TQueryResult>(
+        TQuery query, CancellationToken cancellation);
+}
+
+public interface ICommandHandler<in TCommand, TCommandResult>
+{
+    Task<TCommandResult> Handle(TCommand query, CancellationToken cancellation);
+}
+
+public interface ICommandDispatcher
+{
+    Task<TCommandResult> Dispatch<TCommand, TCommandResult>(
+        TCommand command, CancellationToken cancellation);
+}
