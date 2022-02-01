af1d98159fe53734f2e47c26ca537a9bf7541ac4
Dilan Livera <Dylan_yahampath@yahoo.com>
2022-02-01 21:20:00 +1100

    update readme file

---
+++ asp-dot-net-core-health-checks/src/WeatherApi/MemoryHealthCheck.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace WeatherApi;
+
+public class MemoryHealthCheck : IHealthCheck
+{
+    //private const long Threshold = 1024L * 1024L * 1024L;
+    private const long Threshold = 1000000;
+
+
+    public Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        var allocatedBytesInManagedMemory = GC.GetTotalMemory(forceFullCollection: false);
+        var data = new Dictionary<string, object>()
+        {
+            { "AllocatedBytesInManagedMemory", allocatedBytesInManagedMemory },
+            { "NumberOfTimesGarbageCollectionOccurredForGen0Collection", GC.CollectionCount(0) },
+            { "NumberOfTimesGarbageCollectionOccurredForGen1Collection", GC.CollectionCount(1) },
+            { "NumberOfTimesGarbageCollectionOccurredForGen2Collection", GC.CollectionCount(2) }
+        };
+
+        if (allocatedBytesInManagedMemory > Threshold)
+        {
+            var result = new HealthCheckResult(
+                status: context.Registration.FailureStatus,
+                description: $"Allocated bytes in managed memory is > {Threshold}",
+                data: data);
+
+            return Task.FromResult(result);
+        }
+
+        var healthyResult = new HealthCheckResult(
+            status: HealthStatus.Healthy,
+            description: $"Allocated bytes in managed memory is <= {Threshold}",
+            data: data);
+
+        return Task.FromResult(healthyResult);
+    }
+}
---
+++ asp-dot-net-core-health-checks/src/WeatherApi/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using WeatherApi;
+
+WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddHealthChecks()
+    .AddCheck<MemoryHealthCheck>(
+        name: "memory",
+        failureStatus: HealthStatus.Unhealthy,
+        tags: new[] { "memory" });
+
+WebApplication? app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapHealthChecks(
+    pattern: "/ready",
+    options: new HealthCheckOptions
+    {
+        ResponseWriter = WriteHealthCheckResponseAsync,
+        ResultStatusCodes =
+        {
+            [HealthStatus.Degraded] = StatusCodes.Status500InternalServerError,
+            [HealthStatus.Healthy] = StatusCodes.Status200OK,
+            [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable,
+        },
+        Predicate = registration => registration.Tags.Contains("memory")
+    });
+
+app.MapHealthChecks(
+    pattern: "/healthui",
+    options: new HealthCheckOptions
+    {
+        Predicate = _ => true,
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+    });
+
+app.Run();
+
+static Task WriteHealthCheckResponseAsync(HttpContext httpContext, HealthReport healthReport)
+{
+    httpContext.Response.ContentType = "application/json";
+
+    var dependencyHealthChecks = healthReport.Entries.Select(entry => new
+    {
+        Name = entry.Key,
+        Discription = entry.Value.Description,
+        Status = entry.Value.Status.ToString(),
+        DurationInSeconds = entry.Value.Duration.TotalSeconds.ToString("0:0.00"),
+        Data = entry.Value.Data,
+        Exception = entry.Value.Exception?.Message
+    });
+
+    var healthCheckResponse = new
+    {
+        Status = healthReport.Status.ToString(),
+        TotalCheckExecutionTimeInSeconds = healthReport.TotalDuration.TotalSeconds.ToString("0:0.00"),
+        DependencyHealthChecks = dependencyHealthChecks
+    };
+
+    var serializerOptions = new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+
+    var responseString = JsonSerializer.Serialize(healthCheckResponse, serializerOptions);
+
+    return httpContext.Response.WriteAsync(responseString);
+}
