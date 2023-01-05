cc433cba75309c1ef3414bd977afb885b7af7ef8
Dilan Livera <Dylan_yahampath@yahoo.com>
2023-01-06 10:25:00 +1100

    add http trigger for on payment received

---
+++ azure-functions/Company.FunctionDemoApp/OnPaymentReceived.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.FunctionDemoApp;
+
+public static class OnPaymentReceived
+{
+    [FunctionName("OnPaymentReceived")]
+    public static async Task<IActionResult> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+        HttpRequest request,
+        ILogger logger)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        string name = request.Query["name"];
+
+        var requestBody = await new StreamReader(request.Body).ReadToEndAsync();
+        dynamic data = JsonConvert.DeserializeObject(requestBody);
+        name = name ?? data?.name;
+
+        return name != null
+            ? new OkObjectResult($"Hello, {name}")
+            : new BadRequestObjectResult(
+                "Please pass a name on the query string or in the request body");
+    }
+}
