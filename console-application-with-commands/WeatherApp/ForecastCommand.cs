0f1d8ab561011ade4052958b7f4ba51c683ce68a
Dilan Livera <Dylan_yahampath@yahoo.com>
2023-03-18 22:10:13 +1100

    add forecast command

---
+++ console-application-with-commands/WeatherApp/ForecastCommand.cs
@@ -0,0 +1,37 @@
+﻿using System.CommandLine;
+
+namespace WeatherApp;
+
+internal class ForecastCommand : Command
+{
+    private readonly FakeWeatherService _weather;
+
+    internal ForecastCommand(FakeWeatherService weather)
+        : base(name: "forecast", description: "Get the forecast. Almost always wrong.")
+    {
+        _weather = weather ?? throw new ArgumentNullException(nameof(weather));
+
+        var cityOption = new Option<string>(
+            name: "--city",
+            getDefaultValue: () => _weather.Settings.DefaultCity,
+            description: "The city.");
+        var daysOption = new Option<int>(
+            name: "--days",
+            getDefaultValue: () => _weather.Settings.DefaultForecastDays,
+            description: "Number of days.");
+
+        AddOption(cityOption);
+        AddOption(daysOption);
+
+        this.SetHandler(Execute, cityOption, daysOption);
+    }
+
+    private async Task Execute(string city, int days)
+    {
+        var report = await _weather.Forecast(days, city);
+        foreach (var item in report)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
