cdcf11e14c977b537ec70c39f5f4ad16cb3cd80b
Dilan Livera <Dylan_yahampath@yahoo.com>
2023-03-18 22:21:33 +1100

    setup di

---
+++ console-application-with-commands/WeatherApp/DependencyInjectionSetup.cs
@@ -0,0 +1,28 @@
+﻿using System.CommandLine;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WeatherApp;
+
+internal static class DependencyInjectionSetup
+{
+    internal static IServiceCollection ConfigureServices(IServiceCollection services)
+    {
+        // build config
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .Build();
+
+        // settings
+        services.Configure<FakeWeatherServiceSettings>(configuration.GetSection(key: "Weather"));
+
+        // add commands:
+        services.AddTransient<Command, CurrentCommand>();
+        services.AddTransient<Command, ForecastCommand>();
+
+        // add services:
+        services.AddTransient<FakeWeatherService>();
+
+        return services;
+    }
+}
--- console-application-with-commands/WeatherApp/Program.cs
+++ console-application-with-commands/WeatherApp/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.CommandLine;
+using Microsoft.Extensions.DependencyInjection;
+using WeatherApp;
 
-Console.WriteLine("Hello, World!");
+var services = new ServiceCollection();
+DependencyInjectionSetup.ConfigureServices(services);
+
+await using ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+// entry to run app
+IEnumerable<Command> commands = serviceProvider.GetServices<Command>();
+var rootCommand = new RootCommand(description: "Weather information using a fake weather service.");
+commands.ToList().ForEach(command => rootCommand.AddCommand(command));
+
+await rootCommand.InvokeAsync(args);
