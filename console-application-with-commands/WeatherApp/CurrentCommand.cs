112900eab379919debe6defbbf2fee7844ee5690
Dilan Livera <Dylan_yahampath@yahoo.com>
2023-03-18 22:05:45 +1100

    add current command

---
+++ console-application-with-commands/WeatherApp/CurrentCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.CommandLine;
+
+namespace WeatherApp;
+
+internal class CurrentCommand : Command
+{
+    private readonly FakeWeatherService _weather;
+
+    internal CurrentCommand(FakeWeatherService weather)
+        : base(name: "current", description: "Gets the current temperature.")
+    {
+        _weather = weather ?? throw new ArgumentNullException(nameof(weather));
+
+        var cityOption = new Option<string>(
+            name: "--city",
+            getDefaultValue: () => _weather.Settings.DefaultCity,
+            description: "The city.");
+
+        AddOption(cityOption);
+
+        this.SetHandler(Execute, cityOption);
+    }
+
+    private async Task Execute(string city)
+    {
+        var report = await _weather.GetTemperature(city);
+        Console.WriteLine(report);
+    }
+}
